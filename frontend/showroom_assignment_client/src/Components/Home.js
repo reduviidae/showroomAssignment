08a03b76cd790e0dc3c7c1efcbf55e3dd695ba2b
reduviidae <wiljago@gmail.com>
2019-04-25 18:06:57 -0400

    started frontend

---
+++ frontend/showroom_assignment_client/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Redirect, NavLink } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      Hello world
+    </div>
+  );
+}
+
+export default Home;
