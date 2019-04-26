41f9916eeee0905b68a6aeb84d2649a00c28f0a1
reduviidae <wiljago@gmail.com>
2019-04-25 20:12:45 -0400

    retrieving users from db on client

--- frontend/showroom_assignment_client/src/Components/Users.js
+++ frontend/showroom_assignment_client/src/Components/Users.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import {Redirect} from 'react-router-dom';
+import { API_ROOT, HEADERS } from '../constants';
 
 
-function Users() {
-  return (
-    <div>
-      Show all user
-    </div>
-  );
+class Users extends Component {
+  state ={
+    users: []
+  }
+  componentDidMount () {
+    this.fetchUsers()
+  };
+
+  fetchUsers = () => {
+    fetch(`${API_ROOT}users`, {
+      method: 'GET',
+      headers: HEADERS
+    })
+      .then(r => r.json())
+      .then(users => this.setState({ users }))
+      .catch(err => {
+        console.log(`Error retrieving users: ${JSON.stringify(err)}`);
+      })
+  }
+
+  render () {
+    const userList = this.state.users.map(user => <li>{user.username}</li>)
+    return (
+      <ul>
+        {userList}
+      </ul>
+    );
+  }
 }
 
 export default Users;
---
+++ frontend/showroom_assignment_client/src/constants/index.js
@@ -0,0 +1,6 @@
+export const API_ROOT = "http://localhost:5000/api/v1/";
+
+export const HEADERS = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  };
