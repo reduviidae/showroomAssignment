938ebb1ff6eac5454d7c19efa90a4b8b17fc39f4
reduviidae <wiljago@gmail.com>
2019-04-25 20:25:18 -0400

    retrieving shows from db

--- frontend/showroom_assignment_client/src/Components/Shows.js
+++ frontend/showroom_assignment_client/src/Components/Shows.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import {Redirect} from 'react-router-dom';
+import { API_ROOT, HEADERS } from '../constants';
 
 
-function Shows() {
-  return (
-    <div>
-      Shows
-    </div>
-  );
+class Shows extends Component {
+  state ={
+    shows: []
+  }
+  componentDidMount () {
+    this.fetchShows()
+  };
+
+  fetchShows = () => {
+    fetch(`${API_ROOT}shows`, {
+      method: 'GET',
+      headers: HEADERS
+    })
+      .then(r => r.json())
+      .then(shows => this.setState({ shows }))
+      .catch(err => {
+        console.log(`Error retrieving shows: ${JSON.stringify(err)}`);
+      })
+  }
+
+  render () {
+    const showList = this.state.shows.map(show => <li>{show.title}</li>)
+    return (
+      <ul>
+        {showList}
+      </ul>
+    );
+  }
 }
 
 export default Shows;
