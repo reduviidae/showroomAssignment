439a2140098367caba74de10a5e2b698e77e6d0b
reduviidae <wiljago@gmail.com>
2019-04-28 21:00:25 -0400

    display one show working

--- frontend/showroom_assignment_client/src/Components/Show.js
+++ frontend/showroom_assignment_client/src/Components/Show.js
@@ -1,19 +1,60 @@
 import React, { Component } from "react";
+import capitalize from "lodash/capitalize";
 
 // Import constants
 import { API_ROOT, HEADERS } from "../constants";
 
 class Show extends Component {
   state = {
-      show:{}
-  }
+    show: {}
+  };
 
-  componentDidMount () {
+  componentDidMount() {
     this.fetchShow();
   }
 
+  fetchShow = () => {
+    fetch(`${API_ROOT}show/${this.props.match.params.id}`, {
+      method: "GET",
+      headers: HEADERS
+    })
+      .then(r => r.json())
+      .then(show => this.setState({ show }));
+  };
+
   render() {
-    return <div>Show one show</div>;
+    const show = () => {
+      if (this.state.show.Genre) {
+        return (
+          <div className="showContainer">
+            <div className="imgBox">
+              <img
+                width="100%"
+                src={this.state.show.img_url}
+                alt="show poster"
+              />
+            </div>
+            <div className="text">
+              <h1 className="showText">{this.state.show.title}</h1>
+              <h2 className="showText">
+                {capitalize(this.state.show.Genre.genre_name)}
+              </h2>
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+          "Error displaying show."
+          </div>
+        );
+      }
+    };
+    return (
+      <div>
+      {show()}
+      </div>
+    );
   }
 }
 
